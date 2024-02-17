5ccf33792d44862565e24a4ed09b63693fff7dc0
MichalEder <edermichal.eder@gmail.com>
2024-02-17 09:27:02 +0100

    import shutil added copy method to FileHandler added handle_new_files method to FolderSynchronizer import typing added type hints

--- FolderSync.py
+++ FolderSync.py
@@ -1,26 +1,33 @@
 import os
+import shutil
+from typing import List, Set
 
 
 class FileHandler:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def calculate_hash(self):
+    def calculate_hash(self) -> str:
         # Calculate hash of file
         pass
 
+    def copy(self, destination: str) -> None:
+        shutil.copy2(self.filename, destination)
+
 
 class FolderSynchronizer:
-    def __init__(self, source_folder, replica_folder, log_file, interval):
+    def __init__(self, source_folder: str, replica_folder: str, log_file: str, interval: int):
         self.source_folder = source_folder
         self.replica_folder = replica_folder
         self.log_file = log_file
         self.interval = interval
 
-    def synchronize(self):
+    def synchronize(self) -> None:
         # Define source and replica files with file list of replica and source folder
         source_files = self.get_file_list(self.source_folder)
         replica_files = self.get_file_list(self.replica_folder)
+        source_files: List[FileHandler] = self.get_file_list(self.source_folder)
+        replica_files: List[FileHandler] = self.get_file_list(self.replica_folder)
 
         # Handle new/modified/deleted files
         self.handle_new_files(source_files, replica_files)
@@ -28,9 +35,9 @@ class FolderSynchronizer:
         self.handle_modified_files(source_files, replica_files)
 
 
-    def get_file_list(self, folder):
+    def get_file_list(self, folder: str) -> List[FileHandler]:
         # return list of files in folder
-        files = []
+        files: List[FileHandler] = []
         for filename in os.listdir(folder):
             full_path = os.path.join(folder, filename)
             if os.path.isfile(full_path):
@@ -38,9 +45,16 @@ class FolderSynchronizer:
         return files
 
 
-    def handle_new_files(self, source_files, replica_files):
-        # check existence of file from source in replica if not present copy file
-        pass
+    def handle_new_files(self, source_files: List[FileHandler], replica_files: List[FileHandler]) -> None:
+        # check existence of file from source in replica
+        # source_filenames: Set[str] = {file_obj.filename for file_obj in source_files}
+        replica_filenames: Set[str] = {file_obj.filename for file_obj in replica_files}
+
+        for file in source_files:
+            if file.filename not in replica_filenames:
+                replica_destination = os.path.join(self.replica_folder, os.path.basename(file.filename))
+                file.copy(replica_destination)
+                self.log(f'Copied: {file.filename} to {replica_destination}')
 
     def handle_deleted_files(self, source_files, replica_files):
         # check existence of file in replica
