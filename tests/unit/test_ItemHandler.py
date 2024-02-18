3805fb4f66ee43afeb5c4ddae9d59e7d00ffb1e0
MichalEder <edermichal.eder@gmail.com>
2024-02-18 15:33:05 +0100

    test calculate hash added

--- tests/unit/test_ItemHandler.py
+++ tests/unit/test_ItemHandler.py
@@ -1,6 +1,7 @@
 import unittest
 import tempfile
 import shutil
+import os
 class TestItemHandler(unittest.TestCase):
     def setUp(self):
         self.temp_source = tempfile.mkdtemp()
@@ -19,3 +20,19 @@ class TestItemHandler(unittest.TestCase):
 
         replica_file = os.path.join(self.temp_replica, "test.txt")
         self.assertTrue(os.path.exists(replica_file))
+    def test_calculate_hash(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(b"This is some test data")
+        temp_file.close()
+
+        item = ItemHandler("test_file.txt", temp_file.name)
+
+        with open(temp_file.name, 'rb') as f:
+            data = f.read()
+            expected_hash = hashlib.md5(data).hexdigest()
+
+        calculated_hash = item.calculate_hash()
+
+        self.assertEqual(calculated_hash, expected_hash)
+
+        os.unlink(temp_file.name)
